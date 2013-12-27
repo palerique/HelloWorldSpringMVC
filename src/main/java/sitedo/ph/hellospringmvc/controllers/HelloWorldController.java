59925c5e1bdc84e0651ec564c145c08857f90fc1
Paulo Henrique Lerbach Rodrigues <paulo.rodrigues@4linux.com.br>
2013-12-27 20:30:23 -0200

    Hello World Spring MVC

---
+++ src/main/java/sitedo/ph/hellospringmvc/controllers/HelloWorldController.java
@@ -0,0 +1,21 @@
+package sitedo.ph.hellospringmvc.controllers;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+	public String dizHelloWorld(ModelMap model) {
+
+		model.addAttribute("mensagem", "Hello World! Data atual: " + new Date());
+
+		return "destino";
+	}
+
+}
